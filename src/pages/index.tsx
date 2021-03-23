8b639e5b92bdd71acbbb2382143dde89ccf542b2
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-03-23 17:35:44 -0300

    created SubscribeButton component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
 
@@ -17,6 +18,7 @@ export default function Home() {
             Get access to all the publications <br/>
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding"/>
