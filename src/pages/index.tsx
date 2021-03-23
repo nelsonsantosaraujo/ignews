891ea4b73d7ec13805ba69282b1ca03fa768e72f
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-03-23 12:11:59 -0300

    working with scoped css and install sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <h1 className={styles.title}>
+      Hello World
+    </h1>
   )
 }
