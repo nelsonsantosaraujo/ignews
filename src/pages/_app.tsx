29787bf6ece8758bf0d7addd9f847f8633d183ac
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-03-23 12:44:17 -0300

    Added global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
-import { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
+
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import styles from '../styles/home.module.scss';
 import Head from 'next/head';
 
 export default function Home() {
@@ -7,7 +6,7 @@ export default function Home() {
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
+      <h1>
         Hello World
       </h1>
     </>
