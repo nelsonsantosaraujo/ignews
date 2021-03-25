de18b4b8262d134e1dd7c7dae83f23998ff1c6a6
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-03-25 10:17:34 -0300

    save user on faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import { query as q } from 'faunadb';
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +12,40 @@ export default NextAuth({
       scope: 'read:user',
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try{
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true;
+      } catch {
+        return false;
+      }
+
+
+    }
+  }
 })
