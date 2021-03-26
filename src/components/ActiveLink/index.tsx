4a9b1decceb5d74de05791de3b32e7dced40279b
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-03-26 18:58:50 -0300

    created ActiveLink component to controll header menu active class

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,22 @@
+import { ReactElement, cloneElement } from "react";
+import Link, { LinkProps } from "next/link";
+import { useRouter } from 'next/dist/client/router';
+
+interface ActiveLinkProps extends LinkProps {
+  children: ReactElement;
+  activeClassName: string;
+}
+
+export function ActiveLink({children, activeClassName, ...rest }: ActiveLinkProps) {
+  const { asPath } = useRouter();
+
+  const className = asPath === rest.href ? activeClassName : '';
+
+  return (
+    <Link {...rest}>
+      {cloneElement(children, {
+        className,
+      })}
+    </Link>
+  );
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,20 @@
+import { ActiveLink } from '../ActiveLink';
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
 export function Header() {
+
   return(
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news"/>
         <nav>
-          <a className={styles.active}>
-            Home
-          </a>
-          <a>
-            Posts
-          </a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
