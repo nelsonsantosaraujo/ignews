65021ee8af068a5261d449c29e0d7d24c07fd2c6
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-03-27 17:30:09 -0300

    check if subscription is active

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -40,8 +40,14 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
   const session = await getSession({req});
   const { slug } = params;
 
-  // if(!session){
-  // }
+  if(!session.activeSubscription){
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
 
   const prismic = getPrismicClient(req);
   
