319cf69d722d1ebef701b2411f9d7dff6e3659cd
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-08-03 20:42:19 -0300

    fix useRouter import

--- src/components/ActiveLink/index.tsx
+++ src/components/ActiveLink/index.tsx
@@ -1,6 +1,6 @@
 import { ReactElement, cloneElement } from "react";
 import Link, { LinkProps } from "next/link";
-import { useRouter } from 'next/dist/client/router';
+import { useRouter } from 'next/router';
 
 interface ActiveLinkProps extends LinkProps {
   children: ReactElement;
