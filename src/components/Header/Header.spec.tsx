d30b98faba6f2ed156789ad7723597ae22cb194b
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-08-03 20:50:39 -0300

    Header test structure

---
+++ src/components/Header/Header.spec.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import { Header } from '.';
+
+jest.mock('next/router', () => {
+  return {
+    useRouter() {
+      return {
+        asPath: '/'
+      }
+    }
+  }
+})
+jest.mock('next-auth/client', () => {
+  return {
+    useSession() {
+      return [null, false]
+    }
+  }
+})
+
+describe('ActiveLink component', () => {
+  it('renders correctly', () => {
+    render(
+      <Header />
+    )
+  
+    expect(screen.getByText('Home')).toBeInTheDocument()
+    expect(screen.getByText('Posts')).toBeInTheDocument()
+  })
+})
+
