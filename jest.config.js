8903990da15530eaee6f27001f716aaab24647e5
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-08-03 20:21:26 -0300

    config testing library

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testIgnorePatterns: ["/node_modules/", "/.next/"],
+  setupFilesAfterEnv: [
+    "<rootDir>/src/tests/setupTests.ts",
+  ],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+  },
+  testEnvironment: 'jsdom',
+};
